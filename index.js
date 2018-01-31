0b59a8cd1cf1e7717cf6e00db1bab36bb4cbb410
gcraughwell <garycraughwell@me.com>
2018-01-31 16:08:03 -0500

    heroku postbuild

--- index.js
+++ index.js
@@ -28,5 +28,16 @@ app.use(passport.session());
 //adding the route and callback route for the auth with google
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //express serves up prod assets
+  app.use(express.static('client/build'));
+
+  //express will serve up index.html even if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
